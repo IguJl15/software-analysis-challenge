60b38d64ba9311cc10a9a53960a306e73004aef5
Igor Julliano <igor.julliano2@gmail.com>
2023-12-12 18:40:27 -0300

    feat: basic controllers and views for products

---
+++ VendasSystem/Controllers/MarcaController.cs
@@ -0,0 +1,152 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VendasSystem.Models;
+
+namespace VendasSystem.Controllers
+{
+    public class MarcaController : Controller
+    {
+        private readonly Data.DbContext _context;
+
+        public MarcaController(Data.DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Marca
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Marcas.ToListAsync());
+        }
+
+        // GET: Marca/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var marca = await _context.Marcas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (marca == null)
+            {
+                return NotFound();
+            }
+
+            return View(marca);
+        }
+
+        // GET: Marca/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Marca/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Descricao")] Marca marca)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(marca);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(marca);
+        }
+
+        // GET: Marca/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var marca = await _context.Marcas.FindAsync(id);
+            if (marca == null)
+            {
+                return NotFound();
+            }
+            return View(marca);
+        }
+
+        // POST: Marca/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Descricao")] Marca marca)
+        {
+            if (id != marca.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(marca);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MarcaExists(marca.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(marca);
+        }
+
+        // GET: Marca/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var marca = await _context.Marcas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (marca == null)
+            {
+                return NotFound();
+            }
+
+            return View(marca);
+        }
+
+        // POST: Marca/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var marca = await _context.Marcas.FindAsync(id);
+            if (marca != null)
+            {
+                _context.Marcas.Remove(marca);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MarcaExists(int id)
+        {
+            return _context.Marcas.Any(e => e.Id == id);
+        }
+    }
+}
